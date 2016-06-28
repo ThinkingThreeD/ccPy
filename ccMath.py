6ce0701d1d17a708172b67619fd15d973974cbdc
CALBAC Cyrill <cyrill.calbac@gmail.com>
2016-06-28 11:14:08 +0200

    Create ccMath.py

---
+++ ccMath.py
@@ -0,0 +1,14 @@
+'''
+Personnal Math 3d Module
+'''
+
+def clamp(val, mini, maxi):
+    if val<mini:
+        val = mini
+    elif val > maxi:
+        val = maxi
+                
+    return val
+    
+if __name__ == '__main__':
+  print clamp(45,0,6)
