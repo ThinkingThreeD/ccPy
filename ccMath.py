37dd152bda84d3d2a4f6fbacd1b6ad7fe3b330a0
CALBAC Cyrill <cyrill.calbac@gmail.com>
2016-07-05 23:53:21 +0200

    Update ccMath.py

--- ccMath.py
+++ ccMath.py
@@ -18,24 +18,59 @@ FUNCTIONS:
   
 '''
 
-def clamp(val, min, max):
-   '''
-   --------------------------------
-   if val < min:
-      return min
-   elif val > max:
-      return max
-   else:
-      return val
-   ---------------------------------
-   if val < min: return min
-   elif val > max: return max
-   else:return val
-   --------------------------------
-   '''
-   return min if val < min else max if val > max else val
+def equalZero(a, tol=0.00001):
+    return a >= -tol and a <= tol
 
+def lerp(v1,v2, t):
+    return v1 + (v2 - v1)*t 
 
+def fit(val , oldmin , oldmax , newmin , newmax):
+    d = oldmax - oldmin
+    if (oldmin < oldmax):
+        if (val < oldmin): 
+            return newmin
+        if (val > oldmax): 
+            return newmax
+    else:
+        if (val < oldmax):
+            return newmax
+        if (val > oldmin):
+            return newmin
+            
+    return newmin + (newmax-newmin)*(val-oldmin)/d
     
-if __name__ == '__main__':
-  print clamp(45,0,6)
+def fit01(val, minVal, maxVal):
+    if (val < 0) : return minVal
+    if (val > 1): return maxVal
+    return lerp(minVal, maxVal, val)
+
+
+def clamp(val, minVal, maxVal ):
+    '''This function clamps the input data between the minimum and maximum values'''
+    #return min if val < min else max if val > max else val
+    if val<minVal:
+        val = minVal
+    elif val>maxVal:
+        val = maxVal
+    return val
+
+def smoothstep(edge0, edge1, x):
+    x = clamp((x - edge0)/(edge1 - edge0), 0.0, 1.0);
+    return x*x*(3 - 2*x)
+
+def smooth(min, max, val):
+   if (val <= min) : return 0
+   if (val >= max) : return 1
+   t = max - min
+   if equalZero(t,1e-18) : return 0.5
+   t = (val - min) / t
+   return t*t*(3.0 - 2.0*t)
+
+def smooth01(min, max, val, roll):
+    if (roll > 0):
+        f = smooth(min, max, val)
+        return 1-(pow(1-f,1/roll)) if roll < 1 else pow(f,roll)
+    return 1
+
+
+
