e861a66d1da08f03d4a333a1f2f73af270335df8
CALBAC Cyrill <cyrill.calbac@gmail.com>
2016-07-06 00:03:24 +0200

    Update ccMath.py

--- ccMath.py
+++ ccMath.py
@@ -22,9 +22,11 @@ def equalZero(a, tol=0.00001):
     return a >= -tol and a <= tol
 
 def lerp(v1,v2, t):
+    '''linear interpolation'''
     return v1 + (v2 - v1)*t 
 
 def fit(val , oldmin , oldmax , newmin , newmax):
+    '''    Remaps the value from the old range to the new range and clamps it'''
     d = oldmax - oldmin
     if (oldmin < oldmax):
         if (val < oldmin): 
@@ -40,6 +42,7 @@ def fit(val , oldmin , oldmax , newmin , newmax):
     return newmin + (newmax-newmin)*(val-oldmin)/d
     
 def fit01(val, minVal, maxVal):
+    '''    Just like fit() but assumes value is already in the range 0 to 1.'''
     if (val < 0) : return minVal
     if (val > 1): return maxVal
     return lerp(minVal, maxVal, val)
@@ -55,10 +58,12 @@ def clamp(val, minVal, maxVal ):
     return val
 
 def smoothstep(edge0, edge1, x):
-    x = clamp((x - edge0)/(edge1 - edge0), 0.0, 1.0);
+    '''Smoothstep is an interpolation function where we assume that the left edge is 0, the right edge is 1, and 0 ≤ x ≤ 1 (https://en.wikipedia.org/wiki/Smoothstep)'''
+    x = clamp((x - edge0)/(edge1 - edge0), 0.0, 1.0)
     return x*x*(3 - 2*x)
 
 def smooth(min, max, val):
+    '''Outputs a smoothed value between 0 and 1 as value goes from minimum to maximum.'''
    if (val <= min) : return 0
    if (val >= max) : return 1
    t = max - min
@@ -67,6 +72,7 @@ def smooth(min, max, val):
    return t*t*(3.0 - 2.0*t)
 
 def smooth01(min, max, val, roll):
+    '''Ease-in ease-out curve (with rolloff)'''
     if (roll > 0):
         f = smooth(min, max, val)
         return 1-(pow(1-f,1/roll)) if roll < 1 else pow(f,roll)
